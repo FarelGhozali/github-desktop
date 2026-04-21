0089a9bb0c8ab7411fcb13a9098382c2eba39f6d
Muhammad Farel Al  Ghozali‏‏‎‏‏‎‏‏‎‏‏‎ <75899476+FarelGhozali@users.noreply.github.com>
2026-04-21 18:38:15 +0700

    feat: integrate Reflog UI into App component
    Handles the Reflog popup rendering and menu events in the main application component.

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -166,6 +166,7 @@ import { getRepositoryType } from '../lib/git'
 import { SSHUserPassword } from './ssh/ssh-user-password'
 import { showContextualMenu } from '../lib/menu-item'
 import { UnreachableCommitsDialog } from './history/unreachable-commits-dialog'
+import { Reflog } from './reflog/reflog'
 import { OpenPullRequestDialog } from './open-pull-request/open-pull-request-dialog'
 import { sendNonFatalException } from '../lib/helpers/non-fatal-exception'
 import { createCommitURL } from '../lib/commit-url'
@@ -551,6 +552,8 @@ export class App extends React.Component<IAppProps, IAppState> {
         return this.showFakeMergeSuccessfulBanner()
       case 'show-icon-test-dialog':
         return this.showIconTestDialog()
+      case 'show-reflog':
+        return this.showReflog()
       default:
         return assertNever(name, `Unknown menu event name: ${name}`)
     }
@@ -1036,6 +1039,13 @@ export class App extends React.Component<IAppProps, IAppState> {
 
   private showAbout() {
     this.props.dispatcher.showPopup({ type: PopupType.About })
+  }
+
+  private showReflog() {
+    const repository = this.getRepository()
+    if (repository instanceof Repository) {
+      this.props.dispatcher.showPopup({ type: PopupType.Reflog, repository })
+    }
   }
 
   private async showHistory(
@@ -2668,6 +2678,15 @@ export class App extends React.Component<IAppProps, IAppState> {
       }
       case PopupType.ConfirmRestart: {
         return <ConfirmRestart onDismissed={onPopupDismissedFn} />
+      }
+      case PopupType.Reflog: {
+        return (
+          <Reflog
+            repository={popup.repository}
+            dispatcher={this.props.dispatcher}
+            onDismissed={onPopupDismissedFn}
+          />
+        )
       }
       default:
         return assertNever(popup, `Unknown popup type: ${popup}`)
