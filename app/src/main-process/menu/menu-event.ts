057f0ea90223082e75bf721a473738384c2821d5
Muhammad Farel Al  Ghozali‏‏‎‏‏‎‏‏‎‏‏‎ <75899476+FarelGhozali@users.noreply.github.com>
2026-04-21 18:37:59 +0700

    feat: add Reflog popup type and menu wiring
    Registers the Reflog popup in the application state and adds a 'View Reflog' item to the Repository menu.

--- app/src/main-process/menu/menu-event.ts
+++ app/src/main-process/menu/menu-event.ts
@@ -58,3 +58,4 @@ export type MenuEvent =
   | 'show-test-cherry-pick-conflicts-banner'
   | 'show-test-merge-successful-banner'
   | 'show-icon-test-dialog'
+  | 'show-reflog'
--- app/src/models/popup.ts
+++ app/src/models/popup.ts
@@ -96,6 +96,7 @@ export enum PopupType {
   UnknownAuthors = 'UnknownAuthors',
   TestIcons = 'TestIcons',
   ConfirmRestart = 'ConfirmRestart',
+  Reflog = 'Reflog',
 }
 
 interface IBasePopup {
@@ -423,6 +424,10 @@ export type PopupDetail =
     }
   | {
       type: PopupType.TestIcons
+    }
+  | {
+      type: PopupType.Reflog
+      repository: Repository
     }
   | { type: PopupType.ConfirmRestart }
 
