e4a802e44b5d28e7df12d241e85322ef29b40377
Muhammad Farel Al  Ghozali‏‏‎‏‏‎‏‏‎‏‏‎ <75899476+FarelGhozali@users.noreply.github.com>
2026-04-22 16:36:01 +0700

    feat(ui): implement submodule manager and add submodule dialogs

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -97,6 +97,8 @@ import { UpstreamAlreadyExists } from './upstream-already-exists'
 import { ReleaseNotes } from './release-notes'
 import { DeletePullRequest } from './delete-branch/delete-pull-request-dialog'
 import { CommitConflictsWarning } from './merge-conflicts'
+import { SubmoduleManager } from './submodules/submodule-manager-dialog'
+import { AddSubmodule } from './submodules/add-submodule-dialog'
 import { AppTheme } from './app-theme'
 import { ApplicationTheme } from './lib/application-theme'
 import { RepositoryStateCache } from '../lib/stores/repository-state-cache'
@@ -477,6 +479,8 @@ export class App extends React.Component<IAppProps, IAppState> {
       case 'rebase-branch':
         this.props.dispatcher.incrementMetric('rebaseCurrentBranchMenuCount')
         return this.showRebaseDialog()
+      case 'manage-submodules':
+        return this.showSubmoduleManager()
       case 'show-repository-settings':
         return this.showRepositorySettings()
       case 'view-repository-on-github':
@@ -1427,6 +1431,14 @@ export class App extends React.Component<IAppProps, IAppState> {
     }
 
     this.props.dispatcher.showRebaseDialog(repository)
+  }
+
+  private showSubmoduleManager() {
+    const repository = this.getRepository()
+    this.props.dispatcher.showPopup({
+      type: PopupType.SubmoduleManager,
+      repository,
+    })
   }
 
   private showRepositorySettings() {
@@ -2669,10 +2681,29 @@ export class App extends React.Component<IAppProps, IAppState> {
       case PopupType.ConfirmRestart: {
         return <ConfirmRestart onDismissed={onPopupDismissedFn} />
       }
+      case PopupType.SubmoduleManager:
+        return (
+          <SubmoduleManager
+            key="submodule-manager"
+            dispatcher={this.props.dispatcher}
+            repository={popup.repository}
+            onDismissed={onPopupDismissedFn}
+          />
+        )
+      case PopupType.AddSubmodule:
+        return (
+          <AddSubmodule
+            key="add-submodule"
+            dispatcher={this.props.dispatcher}
+            repository={popup.repository}
+            onDismissed={onPopupDismissedFn}
+          />
+        )
       default:
-        return assertNever(popup, `Unknown popup type: ${popup}`)
-    }
-  }
+        return assertNever(popup, `Unknown popup type: ${popup.type}`)
+      }
+      }
+
 
   private getPullRequestState() {
     const { selectedState } = this.state
---
+++ app/src/ui/submodules/add-submodule-dialog.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react'
+import { Dispatcher } from '../dispatcher'
+import { Repository } from '../../models/repository'
+import { Dialog, DialogContent, DialogFooter } from '../dialog'
+import { OkCancelButtonGroup } from '../dialog/ok-cancel-button-group'
+import { TextBox } from '../lib/text-box'
+import { addSubmodule } from '../../lib/git/submodule'
+
+interface IAddSubmoduleProps {
+  readonly dispatcher: Dispatcher
+  readonly repository: Repository
+  readonly onDismissed: () => void
+}
+
+interface IAddSubmoduleState {
+  readonly url: string
+  readonly path: string
+  readonly loading: boolean
+}
+
+export class AddSubmodule extends React.Component<
+  IAddSubmoduleProps,
+  IAddSubmoduleState
+> {
+  public constructor(props: IAddSubmoduleProps) {
+    super(props)
+    this.state = {
+      url: '',
+      path: '',
+      loading: false,
+    }
+  }
+
+  public render() {
+    return (
+      <Dialog
+        id="add-submodule"
+        title={__DARWIN__ ? 'Add Submodule' : 'Add submodule'}
+        onDismissed={this.props.onDismissed}
+        onSubmit={this.onSubmit}
+        loading={this.state.loading}
+      >
+        <DialogContent>
+          <TextBox
+            label="Repository URL"
+            placeholder="https://github.com/user/repo.git"
+            value={this.state.url}
+            onValueChange={this.onUrlChange}
+            autoFocus={true}
+          />
+          <TextBox
+            label="Local Path"
+            placeholder="vendor/my-submodule"
+            value={this.state.path}
+            onValueChange={this.onPathChange}
+          />
+        </DialogContent>
+        <DialogFooter>
+          <OkCancelButtonGroup
+            okButtonText="Add Submodule"
+            okButtonDisabled={
+              this.state.url.length === 0 || this.state.path.length === 0
+            }
+          />
+        </DialogFooter>
+      </Dialog>
+    )
+  }
+
+  private onUrlChange = (url: string) => {
+    this.setState({ url })
+  }
+
+  private onPathChange = (path: string) => {
+    this.setState({ path })
+  }
+
+  private onSubmit = async () => {
+    this.setState({ loading: true })
+    try {
+      await addSubmodule(
+        this.props.repository,
+        this.state.url,
+        this.state.path
+      )
+      this.props.onDismissed()
+    } catch (e) {
+      this.props.dispatcher.postError(e)
+      this.setState({ loading: false })
+    }
+  }
+}
