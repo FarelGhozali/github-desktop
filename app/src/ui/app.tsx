50d525e24bc8b5cf7d3c25ef14605728ccf165ce
Muhammad Farel Al  Ghozali‏‏‎‏‏‎‏‏‎‏‏‎ <75899476+FarelGhozali@users.noreply.github.com>
2026-04-23 15:01:22 +0700

    feat: add syntax highlighting preference UI
    Add a checkbox to the Appearance tab in Preferences to toggle syntax highlighting.

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -1754,6 +1754,7 @@ export class App extends React.Component<IAppProps, IAppState> {
             selectedShell={this.state.selectedShell}
             selectedTheme={this.state.selectedTheme}
             selectedTabSize={this.state.selectedTabSize}
+            syntaxHighlightingEnabled={this.state.syntaxHighlightingEnabled}
             useCustomEditor={this.state.useCustomEditor}
             customEditor={this.state.customEditor}
             useCustomShell={this.state.useCustomShell}
